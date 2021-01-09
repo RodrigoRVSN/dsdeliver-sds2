dec25e6c4dd3adfa9d2e691df28d53072d463912
RodrigoRVSN <rodrigovictor81@gmail.com>
2021-01-09 14:47:40 -0300

    StepsHeader

---
+++ front-web/src/Orders/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+import StepsHeader from './StepsHeader';
+
+function App(){
+    return(
+        <div className="orders-container">
+            <StepsHeader />
+        </div>
+    )
+}
+export default App;
